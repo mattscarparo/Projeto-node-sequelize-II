e2a222c5a3ca335421f928740ce36bc12b46fc03
Matheus Scarparo <mat.scarparo@gmail.com>
2024-10-24 02:41:08 -0300

    first commit
    Projeto do curso de NodeJs com ORM Sequelize.

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,31 @@
+const Controller = require('./Controller.js');
+const PessoaServices = require('../services/PessoaServices.js');
+
+const pessoaServices = new PessoaServices();
+
+class PessoaController extends Controller {
+  constructor() {
+    super(pessoaServices);
+  }
+
+  async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res.status(200).json(listaMatriculas);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+
+  async pegaTodasAsPessoas(req, res) {
+    try {
+      const listaTodasAsPessoas = await pessoaServices.pegaPessoasEscopoTodos();
+      return res.status(200).json(listaTodasAsPessoas);
+    } catch (erro) {
+      return res.status(500).json({ erro: erro.message });
+    }
+  }
+}
+
+module.exports = PessoaController;
---
+++ src/database/models/pessoa.js
@@ -0,0 +1,66 @@
+'use strict';
+const isCpfValido = require('../../utils/validaCpfHelper.js')
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Pessoa extends Model {
+    static associate(models) {
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: 'docente_id'
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        // scope: { status: 'matriculado' },
+        as: 'aulasMatriculadas'
+      });
+    }
+  }
+  Pessoa.init({
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 90],
+          msg: 'o campo nome deve ter no mínimo 3 caracteres e no máximo 90 caracteres'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'formato do email inválido'
+        }
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if (!isCpfValido(cpf)) throw new Error('numero de CPF inválido');
+        }
+      }
+    },
+    ativo: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Pessoa',
+    tableName: 'pessoas',
+    paranoid: true,
+    defaultScope: {
+      where: {
+        ativo: true,
+      }
+    },
+    scopes: {
+      todosOsRegistros: {
+        where: {}
+      }
+    }
+  });
+  return Pessoa;
+};
